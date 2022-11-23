880961fc318fbe9b455a35019c2fc761e9314ae8
tokyo-walker <nana7mi3goo@gmail.com>
2022-11-23 23:42:19 +0900

    master force commit. add src

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+export default function Numbering() {
+  const [number, setNumber] = useState(0);
+  return (
+    <section className="box">
+      <button className="button" onClick={() => setNumber(number+1)}>+</button>
+      <span className="number">{number}</span>
+      <button className="button" onClick={() => setNumber(number-1)}>-</button>
+    </section>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Numbering/>);
