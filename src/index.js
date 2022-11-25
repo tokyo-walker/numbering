1a82b320d47e61407771f64a04677d0fdb4425c7
tokyo-walker <nana7mi3goo@gmail.com>
2022-11-25 12:29:02 +0900

    add reset btn

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 
 export default function Numbering() {
   const [number, setNumber] = useState(0);
+  const reset = () => {
+    setNumber(0)
+  }
   return (
     <section className="box">
       <button className="button" onClick={() => setNumber(number+1)}>+</button>
       <span className="number">{number}</span>
       <button className="button" onClick={() => setNumber(number-1)}>-</button>
+
+      <button className="button" onClick={() => setNumber(reset)}>reset</button>
     </section>
   );
 }
